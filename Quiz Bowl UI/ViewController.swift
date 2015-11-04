3082e96f4898d5ffcfecb0ffb9c27ff3d4681b2e
MUNNS MATTHEW <818222@ehs-s-itc-25>
2015-11-04 10:38:32 -0600

    Initial Commit

---
+++ Quiz Bowl UI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quiz Bowl UI
+//
+//  Created by MUNNS, MATTHEW on 11/4/15.
+//  Copyright © 2015 MUNNS, MATTHEW. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
