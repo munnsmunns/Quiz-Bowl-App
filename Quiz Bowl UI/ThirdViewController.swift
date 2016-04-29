dcc19e644f963ef285e895564741f2fd234c0382
RAWSON, WYATT <753442@ehs-s-itc-24.local>
2016-04-29 10:42:44 -0500

    This code doesn't work, but it's a start.

---
+++ Quiz Bowl UI/ThirdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.swift
+//  Quiz Bowl UI
+//
+//  Created by RAWSON, WYATT on 4/28/16.
+//  Copyright © 2016 MUNNS, MATTHEW. All rights reserved.
+//
+
+import Foundation
+
+if (Data.teamscore(1) > Data.teamscore(2) {
+print(Team1Name + " beat " + Team2Name + " by " + Data.teamscore(1) - Data.teamscore(2) + " points!")
+}
+else if(Data.Teamscore(2) > Data.TeamScore(1) {
+print(Team2Name + " beat " + Team1Name + " by " + Data.teamscore(2) - Data.teamScore(1) + " points!")
+}
